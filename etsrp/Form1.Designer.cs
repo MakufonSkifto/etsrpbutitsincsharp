20bc79a1398af7f79f85d8b2530172fe2b1fd4b9
MakufonSkifto <63517174+emirsurmen@users.noreply.github.com>
2021-02-03 12:12:42 +0300

    Changed version

--- etsrp/Form1.Designer.cs
+++ etsrp/Form1.Designer.cs
@@ -85,7 +85,7 @@
             this.Icon = ((System.Drawing.Icon) (resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "ETSRP v1.8";
+            this.Text = "ETSRP v2.0";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
         }
